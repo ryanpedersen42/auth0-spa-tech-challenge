8d50b2225c6ffbd1e068f755107161ea64e984a5
Ryan Pedersen <ryanpedersen42@gmail.com>
2019-10-08 20:10:16 -0500

    env adjustment

--- src/index.js
+++ src/index.js
@@ -6,6 +6,8 @@ import * as serviceWorker from "./serviceWorker";
 import { Auth0Provider } from "./react-auth0-spa";
 import config from "./auth_config";
 import history from "./utils/history";
+require('dotenv').config();
+
 
 const onRedirectCallback = appState => {
   history.push(

da30fd4105f63f198085261886998bcc5168f7a0
Ryan Pedersen <ryanpedersen42@gmail.com>
2019-10-09 19:55:38 -0500

    format front end

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,12 +1,16 @@
 import React, { Fragment } from "react";
 
-const Home = () => (
-  <Fragment>
-    <h3>Welcome to Pizza 42's website.</h3>
-    <p>
-      Log in and verify your email to place an order. 
-    </p>
-  </Fragment>
-);
+const Home = () => {
+  const { isAuthenticated } = useAuth0();
+
+  return (
+    <Fragment>
+      <h3>Welcome to Pizza 42's website.</h3>
+      {!isAuthenticated && (
+        <p>Log in and verify your email to place an order.</p>
+      )}
+    </Fragment>
+  );
+};
 
 export default Home;
